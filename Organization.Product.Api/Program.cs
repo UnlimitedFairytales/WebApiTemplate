62012384b777fd8f5c58967975c1030a58fa5fcd
HOME-20230701\Ether <etherealgarden@msn.com>
2023-11-11 18:08:04 +0900

    add note

--- Organization.Product.Api/Program.cs
+++ Organization.Product.Api/Program.cs
@@ -33,3 +33,44 @@ namespace Organization.Product.Api
         }
     }
 }
+
+/*
+________________________________________________________________________________
+# 1. .NET5 から .NET6へのテンプレートの変更点
+________________________________________________________________________________
+ASP.NET Core 6.0 の新しい最小ホスティングモデルに移行されたコードサンプル  
+https://docs.microsoft.com/ja-jp/aspnet/core/migration/50-to-60-samples?view=aspnetcore-6.0
+
+.NET5 Builder factory
+
+|.net 5                             |.net6
+|-----------------------------------|-------------------------------------------
+|WebHost.CreateDefaultBuilder(args) |WebApplication.CreateBuilder(args)
+
+-
+
+.NET5 ConfigureServices()
+
+|.net 5                             |.net6
+|-----------------------------------|-------------------------------------------
+|services.AddControllers();         |builder.Services.AddControllers();
+|-                                  |builder.Services.AddEndpointsApiExplorer();
+|services.AddSwaggerGen(...);       |builder.Services.AddSwaggerGen(...);
+
+-
+
+.NET5 Configure()
+
+|.net 5                             |.net6
+|-----------------------------------|------------------------------------------
+|app.UseDeveloperExceptionPage();   |-
+|app.UseSwagger();                  |app.UseSwagger();
+|app.UseSwaggerUI(...);             |app.UseSwaggerUI(...);
+|app.UseHttpsRedirection();         |app.UseHttpsRedirection();
+|app.UseRouting();                  |-
+|app.UseAuthorization();            |app.UseAuthorization();
+|app.UseEndpoints(x=>{x.MapControllers();}|app.MapControllers();
+
+-
+
+*/
