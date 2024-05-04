adee8f03bcf2f4867671edc3b10e4c6766a673b7
HOME-20230701\Ether <etherealgarden@msn.com>
2024-05-04 23:22:33 +0900

    Thread safe

--- Organization.Product.Api/Utils/MyJavaScriptEncoder.cs
+++ Organization.Product.Api/Utils/MyJavaScriptEncoder.cs
@@ -3,30 +3,27 @@ using System.Text.Unicode;
 
 namespace Organization.Product.Api.Utils
 {
+    // Thread safe singleton
+    // https://csharpindepth.com/Articles/Singleton
     public class MyJavaScriptEncoder
     {
-        static JavaScriptEncoder? _japanese;
-        public static JavaScriptEncoder Japanese
-        {
-            get
-            {
-                // 「<>&'"+」「\`」はエスケープされる
-                _japanese ??= JavaScriptEncoder.Create(
-                    UnicodeRanges.BasicLatin,
-                    UnicodeRanges.CjkRadicalsSupplement, //          2E80-2EFF
-                    UnicodeRanges.KangxiRadicals, //                 2F00-2FDF
-                    UnicodeRanges.CjkSymbolsandPunctuation, //       3000-303F
-                    UnicodeRanges.Hiragana, //                       3040-309F
-                    UnicodeRanges.Katakana, //                       30A0-30FF
-                    UnicodeRanges.KatakanaPhoneticExtensions, //     31F0-31FF
-                    UnicodeRanges.EnclosedCjkLettersandMonths, //    3200-32FF
-                    UnicodeRanges.CjkCompatibility, //               3300-33FF
-                    UnicodeRanges.CjkUnifiedIdeographsExtensionA, // 3400-4DB5
-                    UnicodeRanges.CjkUnifiedIdeographs, //           4E00-9FCC
-                    UnicodeRanges.CjkCompatibilityIdeographs, //     F900-FAD9
-                    UnicodeRanges.HalfwidthandFullwidthForms); //    FF00-FFEE
-                return _japanese;
-            }
-        }
+        // 「<>&'"+」「\`」はエスケープされる
+        public static readonly JavaScriptEncoder Japanese = JavaScriptEncoder.Create(
+            UnicodeRanges.BasicLatin,
+            UnicodeRanges.CjkRadicalsSupplement, //          2E80-2EFF
+            UnicodeRanges.KangxiRadicals, //                 2F00-2FDF
+            UnicodeRanges.CjkSymbolsandPunctuation, //       3000-303F
+            UnicodeRanges.Hiragana, //                       3040-309F
+            UnicodeRanges.Katakana, //                       30A0-30FF
+            UnicodeRanges.KatakanaPhoneticExtensions, //     31F0-31FF
+            UnicodeRanges.EnclosedCjkLettersandMonths, //    3200-32FF
+            UnicodeRanges.CjkCompatibility, //               3300-33FF
+            UnicodeRanges.CjkUnifiedIdeographsExtensionA, // 3400-4DB5
+            UnicodeRanges.CjkUnifiedIdeographs, //           4E00-9FCC
+            UnicodeRanges.CjkCompatibilityIdeographs, //     F900-FAD9
+            UnicodeRanges.HalfwidthandFullwidthForms); //    FF00-FFEE
+
+        // Do not mark as beforefieldinit
+        static MyJavaScriptEncoder() { }
     }
 }
