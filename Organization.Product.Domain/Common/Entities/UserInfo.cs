dc17d71a36be934ea30a8b1a4c8cef7b571491b7
HOME-20230701\Ether <etherealgarden@msn.com>
2024-05-04 14:45:36 +0900

    Add note

--- Organization.Product.Domain/Common/Entities/UserInfo.cs
+++ Organization.Product.Domain/Common/Entities/UserInfo.cs
@@ -2,8 +2,18 @@
 {
     public class UserInfo
     {
+        public long? UserId { get; set; }
         public string? UserCd { get; set; }
         public string? UserName { get; set; }
         public string? Email { get; set; }
+
+        public long? Ver { get; set; }
+
+        // AuthZ
+        // Menu list
+        // Sub menu list
+        // App Roles / App privilege group
+        // App privilege
+        // Other flags...
     }
 }
