5f44b090ceebe597d9afccb3c937a87b436fb5de
HOME-20230701\Ether <etherealgarden@msn.com>
2024-05-07 02:20:27 +0900

    Clear

--- Organization.Product.Domain/Authentications/Services/IHasher.cs
+++ Organization.Product.Domain/Authentications/Services/IHasher.cs
@@ -1,17 +1,7 @@
-﻿using Organization.Product.Domain.Common.ValueObjects;
-
-namespace Organization.Product.Domain.Authentications.Services
+﻿namespace Organization.Product.Domain.Authentications.Services
 {
     public interface IHasher
     {
         string Hash(string text, string salt);
-
-        public static void Validate(string userCd, string password, IHasher hasher)
-        {
-            var salt = "this is salt3456";
-            var hashed = hasher.Hash(password, salt);
-            var isValid = userCd == "USER00" && hashed == hasher.Hash("USER00", salt);
-            if (!isValid) throw AppException.Create(AppMessage.W5001());
-        }
     }
 }
